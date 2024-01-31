3ff4146bbd38854df497910731b3473ebb6a4d6a
princetonbishop <princetonbishop@protonmail.com>
2024-01-31 11:53:00 +0100

    adding contract files

---
+++ protocol/scripts/deploys/arbitrumSepolia/deploymentArgs/0xDec7cdBd4Ef07c6F540466e33F2F560c51Ae93ea.js
@@ -0,0 +1,3 @@
+// arbitrumSepolia: CODE_GENERATOR=0xDec7cdBd4Ef07c6F540466e33F2F560c51Ae93ea
+// yarn hardhat verify --network arbitrumSepolia 0xDec7cdBd4Ef07c6F540466e33F2F560c51Ae93ea --constructor-args scripts/deploys/arbitrumSepolia/deploymentArgs/0xDec7cdBd4Ef07c6F540466e33F2F560c51Ae93ea.js
+module.exports = [];
